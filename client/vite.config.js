e679c841a35c7393efb2b2979e3b81f1290d0dcc
Sharma Abhishek <abhis70411@gmail.com>
2024-12-13 16:47:39 +0530

    fifth commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the chunk size warning limit (in kibibytes)
+    chunkSizeWarningLimit: 1000, // Default is usually around 500
+  }
 })
