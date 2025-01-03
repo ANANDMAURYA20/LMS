3147066dd57fe8be643e50848f9b68dd79b05537
Sharma Abhishek <abhis70411@gmail.com>
2025-01-03 19:42:53 +0530

    blog loader

--- client/src/Pages/Blog.jsx
+++ client/src/Pages/Blog.jsx
@@ -29,6 +29,16 @@ const Blog = () => {
     fetchBlogs();
   }, []);
 
+  if (loading) {
+    return (
+      <Layout>
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-purple-500"></div>
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
 
