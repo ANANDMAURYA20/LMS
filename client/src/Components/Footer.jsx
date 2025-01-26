75b3a9e73b72fd910b531fb6df1db528f135c2b7
Sharma Abhishek <abhis70411@gmail.com>
2025-01-26 15:22:47 +0530

    whatsapp link

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsTwitter, BsWhatsapp } from "react-icons/bs";
 
 export default function Footer() {
   const curDate = new Date();
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="py-12 md:px-16 px-3 bg-slate-100 dark:bg-gray-900 flex md:flex-row flex-col md:justify-between justify-center items-center gap-4">
       <span className="md:text-xl  text-lg font-[600] text-gray-700 dark:text-white">
-        Copyright {year} | All Right Reserved <a href="https://sharmaabhi7.github.io/portfolio/#">Abhishek Sharma</a>
+        Copyright {year} | All Right Reserved Lyceum Digisolutions Pvt Ltd (OPC)
       </span>
       <div className="flex gap-5 items-center">
         <a
@@ -23,10 +23,10 @@ export default function Footer() {
           <BsInstagram />
         </a>
         <a
-          href="https://www.linkedin.com/in/abhishek-sharma-46b635295/"
+          href="https://api.whatsapp.com/send?phone=+916394499123%20&%20text=Hi%20I%20"
           className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
         >
-          <BsLinkedin />
+          <BsWhatsapp />
         </a>
         <a
           href="#"
